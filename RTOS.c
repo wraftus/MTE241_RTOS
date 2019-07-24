ec583c3ecbcaa8c0179b81a0d25f9b69ea935611
Walter Raftus <35929488+wraftus@users.noreply.github.com>
2019-07-24 16:55:41 -0400

    Revert "Semaphore and mutex"

--- RTOS.c
+++ RTOS.c
@@ -17,45 +17,27 @@ TCB_t TCBList[MAX_NUM_TASKS];
 TCB_t *runningTCB;
 TCB_t *readyListHead;
 
-void addToList(TCB_t *toAdd, TCB_t **listHead){
-	if(*listHead == NULL){
-		*listHead = toAdd;
+void addToReadyList(TCB_t *tcb){
+	if(readyListHead == NULL){
+		readyListHead = tcb;
 	}
 	else{
-		TCB_t *temp = *listHead;
-		while(temp->next != NULL){
-			temp = temp->next;
+		TCB_t *readyTCB = readyListHead;
+		while(readyTCB->next != NULL){
+			readyTCB = readyTCB->next;
 		}
-		temp->next = toAdd;
+		readyTCB->next = tcb;
 	}
-}
-
-void removeFromList(TCB_t *toRemove, TCB_t **listHead){
-	//TODO maybe make this boolean incase it's not in the list?
-	TCB_t *temp = *listHead;
-	while(temp != NULL && temp->next != toRemove){
-		temp = temp->next;
-	}
-	if(temp == NULL){
-		//could not find toRemove
-		return;
-	}
-	//TODO could break if toRemove is NULL (not sure why this would be the case tho...)
-	temp->next = temp->next->next;
 }
 
 void SysTick_Handler(void) {
     rtosTickCounter++;
-    if(rtosTickCounter - nextTimeSlice >= TIME_SLICE_TICKS || runningTCB->state == WAITING){
+    if(rtosTickCounter - nextTimeSlice >= TIME_SLICE_TICKS){
     	//we are ready to move to the next task
-    	if(runningTCB->state == WAITING){
-    		rtosTickCounter = nextTimeSlice;
-    	}
     	nextTimeSlice += TIME_SLICE_TICKS;
-		//TODO unsure what to do if all tasks are waiting
     	if(readyListHead != NULL){
     		//notify PendSV_Handler we are ready to switch
-			SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
+				SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
     	}
     }
 }
@@ -67,9 +49,7 @@ void PendSV_Handler(void){
 	
 	//queue the current running task, pop next task
 	//TODO change state of running task to ready
-	if(runningTCB0->state != WAITING){
-		addToList(runningTCB, readyListHead);
-	}
+	addToReadyList(runningTCB);
 	runningTCB = readyListHead;
 	readyListHead = readyListHead->next;
 	runningTCB->next = NULL;
@@ -83,7 +63,7 @@ void PendSV_Handler(void){
 void rtosInit(void){
 	for(uint8_t i = 0; i < MAX_NUM_TASKS; i++){
 		//initialize each TCB with their stack number and base stack adress
-		TCBList[i].id = i;
+		TCBList[i].stackNum = i;
 		TCBList[i].stackPointer = TCBList[i].baseOfStack = *((uint32_t *)SCB->VTOR) - MAIN_TASK_SIZE - TASK_STACK_SIZE * (MAX_NUM_TASKS - 1 - i);
 		TCBList[i].next = NULL;
 		TCBList[i].state = SUSPENDED;
@@ -142,62 +122,32 @@ void rtosThreadNew(rtosTaskFunc_t func, void *arg){
 
 	//set current task to ready and put it in the list
 	newTCB->state = READY;
-	addToList(newTCB, readyListHead);
+	addToReadyList(newTCB);
 
 	//bump up num tasks
 	numTasks++;
 }
 
-void semaphorInit(semaphore_t *sem, uint8_t count){
-	sem->count = count;
-	sem->waitListHead = NULL;
+void semaphorInit(semaphore_t *sem, uint32_t val){
+
 }
 
 void waitOnSemaphor(semaphore_t *sem){
-	__disable_irq();
-	if(sem->count == 0){
-		//semaphore is closed, wait until it is signalled
-		addToList(runningTCB, sem->waitListHead);
-		runningTCB->state = WAITING;
-	}
-	else{
-		sem->count--;
-	}
-	__enable_irq();
+
 }
 
 void signalSemaphor(semaphore_t *sem){
-	__disable_irq();
-	sem->count++;
-	if(sem->waitListHead != NULL){
-		addToList(sem->waitListHead, readyListHead);
-		sem->waitListHead->state = READY;
-		sem->waitListHead = sem->waitListHead->next;
-	}
-	__enable_irq();
+
 }
 
 void mutextInit(mutex_t *mutex){
-	*mutex = -1;
-}
-
-void aquireMutex(mutex_t *mutex){
-	__disable_irq();
-	while(*mutex != -1){
-		__enable_irq();
-		__disable_irq();
-	}
-	*mutex = runningTCB->id;
-	__enable_irq();
 
 }
 
-void releaseMutex(mutex_t *mutex){
-	if(*mutex != runningTCB->id){
-		//cannot release a mutex you do not own
-		return;
-	}
-	__disable_irq();
-	*sem = -1;
-	__enable_irq();
+void waitOnMutex(mutex_t *mutex){
+
+}
+
+void signalMutex(mutex_t *mutex){
+
 }
